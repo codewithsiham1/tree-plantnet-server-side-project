3039623d0828d80ddc4337565822c2f17ae5c68a
moudud <sihamislam18@gmail.com>
2025-07-30 18:06:50 +0600

    add the link page

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:5173', 
-    'https://plantnet-1ba23.web.app'
+    'https://tree-plantnet-client-project.onrender.com'
     
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
